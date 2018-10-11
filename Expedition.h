df3bdfaf09ace08211835d7973ebbb29dd4882cf
Ekalawen <alexys.dussier@gmail.com>
2018-10-11 12:04:24 -0400

    ajout des classes Exp

---
+++ Expedition.h
@@ -0,0 +1,11 @@
+
+#ifndef EXPEDITION_H
+#define EXPEDITION_H
+
+#include "BehaviorTree/BT_Noeud.h"
+
+class Expedition : public BT_Noeud {
+
+};
+
+#endif
---
+++ Exploration.cpp
@@ -0,0 +1,2 @@
+
+#include "Exploitation.h"
