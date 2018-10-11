a61080dfe9230fa49bb6bfbc2638d836d6fb6e1d
ma2tt <matth.polo@gmail.com>
2018-10-11 12:32:16 -0400

    create Strategies folder

---
+++ Strategies/Expedition.h
@@ -0,0 +1,11 @@
+
+#ifndef EXPEDITION_H
+#define EXPEDITION_H
+
+#include "MyBotLogic/BehaviorTree/BT_Noeud.h"
+
+class Expedition : public BT_Noeud {
+
+};
+
+#endif
---
+++ Strategies/Exploration.h
@@ -0,0 +1,10 @@
+#ifndef EXPLORATION_H
+#define EXPLORATION_H
+
+#include "MyBotLogic/BehaviorTree/BT_Noeud.h"
+
+class Exploration : public BT_Noeud {
+
+};
+
+#endif
