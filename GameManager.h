78370d29c998ef73dd930bbd63ae7775a168f0ad
ma2tt <matth.p@pm.me>
2018-10-11 11:43:04 -0400

    Merge conflits resolved

--- GameManager.h
+++ GameManager.h
@@ -4,11 +4,8 @@
 #include "Map.h"
 #include "Npc.h"
 #include "Logger.h"
-<<<<<<< HEAD
 #include "Mouvement.h"
-=======
 #include "TurnInfo.h"
->>>>>>> cd663c462ad2249adf525ba74dfe1a1b89a746a9
 #include <map>
 
 class GameManager {
@@ -21,12 +18,8 @@ public:
     GameManager(LevelInfo);
     void associateNpcsWithObjectiv();
     void moveNpcs(vector<Action*>& actionList); // Remplie l'action liste !
-<<<<<<< HEAD
     void ordonnerMouvements(vector<Mouvement*>& mouvements); // Permet d'ordonner les mouvements pour éviter les collisions et gérer les politesses de priorités =)
-=======
-    void ordonnerMouvements(vector<tuple<int, Tile::ETilePosition>>& mouvements); // Permet d'ordonner les mouvements pour éviter les collisions et gérer les politesses de priorités =)
     void updateModel(TurnInfo); // Met à jour le modèle avec les informations que découvrent les NPCS
->>>>>>> cd663c462ad2249adf525ba74dfe1a1b89a746a9
 
     static void Log(string str) { // Permet de débugger ! :D
         logger.Log(str);
