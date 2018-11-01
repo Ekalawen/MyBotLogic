acc7505bdd22f7c6019b33f579c18e197f941947
guiguibubu <guibu44@hotmail.fr>
2018-11-01 15:55:03 -0400

    changement Map.h en Carte.h

--- Carte.h
+++ Carte.h
@@ -8,6 +8,7 @@
 #include "Npc.h"
 #include "MyBotLogic/MapTile.h"
 #include <map>
+#include <vector>
 
 class tile_inexistante {};
 
@@ -25,32 +26,31 @@ class Carte {
     std::map<unsigned int, ObjectInfo> activateurs;
 
 public:
-
     Carte() = default;
     Carte(const LevelInfo);
-    bool isInMap(int _idTile) const noexcept;
+    bool isInMap(const int _idTile) const noexcept;
     std::vector<unsigned int> getObjectifs() const noexcept;
 
-    Chemin aStar(int _depart, int _arrivee, float _coefEvaluation = 1) noexcept; // Renvoie le chemin à parcourir pour aller du départ à l'arrivée
+    Chemin aStar(const int _depart, const int _arrivee, const float _coefEvaluation = 1) const noexcept; // Renvoie le chemin à parcourir pour aller du départ à l'arrivée
 
-    float distanceL2(int _depart, int _arrivee) const noexcept; // Renvoie la distance L2 à vol d'oiseau !
-    int distanceHex(int _depart, int _arrivee) const noexcept;
-    Tile::ETilePosition getDirection(int _tileFrom, int _tileTo) const noexcept; // Permet de savoir dans quel sens se déplacer pour aller d'une tile à l'autre si celles-ci sont adjacentes ! =)
-    int Carte::getAdjacentTileAt(int _tileSource, Tile::ETilePosition _direction) const noexcept; // Permet de récupérer l'indice d'une tuile adjacente à une autre
+    float distanceL2(const int _depart, const int _arrivee) const noexcept; // Renvoie la distance L2 à vol d'oiseau !
+    int distanceHex(const int _depart, const int _arrivee) const noexcept;
+    Tile::ETilePosition getDirection(const int _tileFrom, const int _tileTo) const noexcept; // Permet de savoir dans quel sens se déplacer pour aller d'une tile à l'autre si celles-ci sont adjacentes ! =)
+    int Carte::getAdjacentTileAt(const int _tileSource, const Tile::ETilePosition _direction) const noexcept; // Permet de récupérer l'indice d'une tuile adjacente à une autre
     int tailleCheminMax() const noexcept; // Permet de savoir la taille maximum d'un chemin
 
-    void addTile(TileInfo) noexcept; // Permet de rajouter une tile à la map
-    void addObject(ObjectInfo) noexcept; // Permet de rajouter un object à la map
+    void addTile(const TileInfo&) noexcept; // Permet de rajouter une tile à la map
+    void addObject(const ObjectInfo&) noexcept; // Permet de rajouter un object à la map
 
-    int getX(int _id) const noexcept; // Permet de récupérer x et y à partir d'un indice
-    int getY(int _id) const noexcept;
-    std::vector<int> getVoisins(int _id) const noexcept;
+    int getX(const int _id) const noexcept; // Permet de récupérer x et y à partir d'un indice
+    int getY(const int _id) const noexcept;
 
     int getRowCount() const noexcept;
     int getColCount() const noexcept;
     int getNbTiles() const noexcept;
     int getNbTilesDecouvertes() const noexcept;
-    MapTile& getTile(int _id);
+    MapTile& getTile(const int _id);
+    const MapTile& getTile(const int id) const;
 
     std::vector<unsigned int> getObjectifs();
     std::map<unsigned int, ObjectInfo> getMurs();
@@ -58,7 +58,7 @@ public:
     std::map<unsigned int, ObjectInfo> getFenetres();
     std::map<unsigned int, ObjectInfo> getActivateurs();
 
-    bool objectExist(int id); // Permet de savoir si un objet existe déjà ou pas
+    bool objectExist(const int _id)const noexcept; // Permet de savoir si un objet existe déjà ou pas
 };
 
 
